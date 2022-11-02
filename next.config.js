34b429d194a2f702c0aad91ed2eeb699c95c0bb9
Harris02 <khairulihsannudin704@gmail.com>
2022-11-02 19:57:03 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = {
+  images: {
+    domains: ['creativemj.com'],
+  },
+}
+module.exports = nextConfig
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+export default function(){
+    return(
+        <h1>Inbound OmahTI</h1>
+    )
+}
