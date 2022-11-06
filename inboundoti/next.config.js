929bd1eab37c1a22385fb53c40784c26eb3134c4
Harris02 <khairulihsannudin704@gmail.com>
2022-11-06 10:40:33 +0700

    kedua

--- inboundoti/next.config.js
+++ inboundoti/next.config.js
@@ -2,9 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = {
-  images: {
-    domains: ['creativemj.com'],
-  },
-}
+
 module.exports = nextConfig
--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function(){
-    return(
-        <h1>Inbound OmahTI</h1>
-    )
-}
